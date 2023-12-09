9a2163bc3c3ea5553c2d78be08bfe9abe6dd5269
John <birukovie@gmail.com>
2023-12-09 22:58:15 +0300

    add views

--- manual/drf/urls.py
+++ manual/drf/urls.py
@@ -2,66 +2,18 @@ from django.urls import path, include
 from .views import *
 from rest_framework.routers import SimpleRouter, DefaultRouter
 
-"""
-PART 1 ListCreateAPIView
-"""
 
-# urlpatterns = [
-#     path('tasks/', TasksAPIView.as_view()),
-#     path('tasks/', TaskListCreateAPIView.as_view()),
-#     path('tasks/', ListCreateAPIView.as_view(queryset=Task.objects.all(), serializer_class=TaskSerializer)),
-# ]
-
-"""
-PART 2 GenericAPIView
-"""
-
-# urlpatterns = [
-#     path('tasks/', TaskGenericAPIView.as_view()),
-# ]
-
-"""
-PART 3 ListAPIView, 
-"""
-
-# urlpatterns = [
-#     path('tasks/', TaskListAPIView.as_view()),
-#     path('tasks/<int:pk>/', TaskDetailAPIView.as_view()),
-# ]
-
-
-"""
-PART 4 Viewset, ModelViewset
-"""
-
-# router = DefaultRouter()
-# router.register(r'tasks', TaskViewset, basename='task')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-# router = DefaultRouter()
-# router.register(r'tasks', TaskModelViewset, basename='task')
-# router.register(r'track', TrackAPIView, basename='track')
-# router.register(r'albums', AlbumAPIView, basename='album')
-
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'users2', UserModelViewSet, basename='user2')
+# ИЛИ
+# user_list = UserViewSet.as_view({'get': 'list'})
+# user_detail = UserViewSet.as_view({'get': 'retrieve'})
 
 
 urlpatterns = [
     path('views/', ListUsers.as_view()),
     path('api_view/', api_view_func),
     path('ListCreateAPIView/', UserList2.as_view()),
-
-    # path('', test),
-    # path('', include(router.urls)),
-    # path('album', AlbumListAPI.as_view())
+    path('ViewSet/', include(router.urls)),
 ]
-
-
-# from rest_framework.request import Request
-# from rest_framework.response import Response
--- manual/drf/views.py
+++ manual/drf/views.py
@@ -1,15 +1,16 @@
-from rest_framework import views
+from rest_framework import (
+    views, mixins, generics, authentication, permissions, viewsets
+)
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.schemas import AutoSchema
-from rest_framework import generics
 from rest_framework.decorators import (
     api_view, throttle_classes, authentication_classes, permission_classes,
-    schema
+    schema, action
 )
 
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from .serializers import UserSerializer
 
@@ -52,10 +53,35 @@ class UserList2(generics.ListCreateAPIView):
         return Response(serializer.data)
 
 
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 
+class UserModelViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+    def get_permissions(self):
+        print(self.action)
+        permission_classes = []
+        # if self.action == 'list':
+        #     permission_classes = []
+        # else:
+        #     permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
 
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticatedOrReadOnly])
+    def new_method(self, request):
+        return Response({'content': 'test'})
 
 
 
